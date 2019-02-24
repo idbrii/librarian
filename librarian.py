adbe952dd8bf3d518c481c22ee56e4e4cc9ec3a8
David Briscoe <idbrii@gmail.com>
2019-02-24 00:42:18 -0800

    Document git dependency

--- librarian.py
+++ librarian.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+# Requires gitpython
+#   pip3 install gitpython
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
