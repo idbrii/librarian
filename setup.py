3763ce838c86f5df6ca29de749948f0cbe611683
David Briscoe <idbrii@gmail.com>
2021-11-25 08:05:12 -0800

    Move requirements from txt to setup.py
    I don't want to maintain two lists and I don't have enough to
    worry about general users vs developers.

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/idbrii/librarian",
     packages=setuptools.find_packages(),
+    install_requires=["GitPython>=3.1.18"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
