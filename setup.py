66d8e1457ebb204d2ffac29eeca361753204807f
David Briscoe <idbrii@gmail.com>
2021-11-25 07:52:28 -0800

    Setup for packaging
    Only packaging it as a script and no python modules so I don't need to
    worry about namespaces.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="code-librarian",
+    version="0.1",
+    scripts=["src/librarian"],
+    author="David Briscoe",
+    #~ author_email="idbrii@users.noreply.github.com",
+    description="Copy dependencies into your project, apply updates, and extract fixes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/idbrii/librarian",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
